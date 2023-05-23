14519c9d510981b687911f5dd6a8e483b5b5b397
Mei30 <meisamjafarimosleh@gmail.com>
2023-05-23 20:25:42 +0330

    Add communication between components

---
+++ components/components/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Greeting from '@/components/Greeting.vue';
+import User from '@/components/User.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      age: 20
+    }
+  },
+  methods: {
+    ageChanged(number) {
+      this.age += number;
+    }
+  },
+  components: {
+    Greeting,
+    User
+  }
+}
+</script>
+
+<template>
+  <p>Hey {{age}}!</p>
+  <Greeting :age="age"></Greeting>
+  <User :age="age" @increment_age="ageChanged"></User>
+</template>
+
+<style>
+</style>
---
+++ components/components/src/components/Greeting.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'greeting',
+  data() {
+    return {
+      message: "Hello World from simple component"
+    }
+  },
+  props: ["age"]
+}
+</script>
+
+<template>
+  <p v-if="age > 20">{{message}}</p>
+</template>
+
+<style scoped lang="scss">
+p {
+  color: darken($color: #e29595, $amount: 15%)
+}
+</style>
