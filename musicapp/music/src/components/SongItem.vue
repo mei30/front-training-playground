7fb8b2569f138986bd13f62e9b2733b36c8100ce
mei30 <meisamjafarimosleh@gmail.com>
2023-06-02 20:48:06 +0330

    Add song page

--- musicapp/music/src/components/SongItem.vue
+++ musicapp/music/src/components/SongItem.vue
@@ -3,8 +3,12 @@
     class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
   >
     <div>
-      <a href="#" class="font-bold block text-gray-600">Song Title</a>
-      <span class="text-gray-500 text-sm">{{ song.modified_name }}</span>
+      <RouterLink
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="font-bold block text-gray-600"
+        >{{ song.modified_name }}</RouterLink
+      >
+      <span class="text-gray-500 text-sm">{{ song.name }}</span>
     </div>
 
     <div class="text-gray-600 text-lg">
