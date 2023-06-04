4a5951fbe56c42683f8ac61d4c96f980d4fe94f9
mei30 <meisamjafarimosleh@gmail.com>
2023-06-04 18:36:07 +0330

    Put finishing touches

--- musicapp/music/src/App.vue
+++ musicapp/music/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <Header></Header>
 
-  <RouterView></RouterView>
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>
 
   <AppPlayer></AppPlayer>
 
@@ -36,3 +40,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
--- musicapp/music/src/components/SongItem.vue
+++ musicapp/music/src/components/SongItem.vue
@@ -12,10 +12,15 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        15
-      </span>
+      <RouterLink
+        :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click.prevent="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </RouterLink>
     </div>
   </li>
 </template>
